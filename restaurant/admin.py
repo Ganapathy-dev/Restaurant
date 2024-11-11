3bbeef8abe31cc2d6037b7dbcb951dfd358f0e4c
Ganapathy <ganapathy@testpress.in>
2024-11-11 14:46:16 +0530

    Complete Models with test cases
    - Implement Restaurant,Dish,Review,BookmarkedRestaurant,visitedRestaurant
    - validate the model, methods and constraint used

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Restaurant
 # Register your models here.
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display=['title','location','cost_of_two','rating','is_spotlight']
+    list_filter=['is_spotlight','food_type','cuisines']
+    search_fields=['title','location',]
+    list_editable=['cost_of_two']
+    fields=[('title','location'),'address',('open_time','close_time'),('rating','cost_of_two'),('food_type','cuisines'),('is_spotlight','owner')]
+
