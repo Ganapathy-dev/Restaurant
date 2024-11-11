b45edd35b7f9cf2c0c2b53d548b79d3906d26628
Ganapathy <ganapathy@testpress.in>
2024-11-11 15:11:25 +0530

    Included Dish and Review models in Admin site.

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Restaurant
+from .models import Restaurant,Dish,Review
 # Register your models here.
 
 @admin.register(Restaurant)
@@ -10,3 +10,12 @@ class RestaurantAdmin(admin.ModelAdmin):
     list_editable=['cost_of_two']
     fields=[('title','location'),'address',('open_time','close_time'),('rating','cost_of_two'),('food_type','cuisines'),('is_spotlight','owner')]
 
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display=['restaurant','name','food_type','price','cuisine']
+    list_filter=['food_type','cuisine']
+    search_fields=['name','restaurant']
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display=['restaurant','user','comment','rating']
